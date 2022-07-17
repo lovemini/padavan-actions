dbe2be170aedc2edcca39c08368ef22c40faa988
LaiQE <laiqe@qq.com>
2022-07-17 14:19:56 +0800

    Update diy-part1.sh
    overclock 1100Mhz to 1000Mhz

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,8 +4,16 @@ sed -i '/WIREGUARD/d' configs/templates/RM2100.config
 sed -i '/MENTOHUST/d' configs/templates/RM2100.config
 sed -i '/TUNSAFE/d' configs/templates/RM2100.config
 
-sed -i 's#0xc2#0x362#' linux-3.4.x/arch/mips/rt2880/init.c
+# 超频:Padavan-KVR
+# 此处填写对应频率的十六进制：1000Mhz=0x312 1100Mhz=0x362 1120Mhz=0x372 1200Mhz=0x3B2
+sed -i 's#0xc2#0x312#' linux-3.4.x/arch/mips/rt2880/init.c
 echo "CONFIG_FIRMWARE_CPU_900MHZ=y" >> configs/templates/RM2100.config
+# 超频:padavan-4.4
+# sed -i 's#CONFIG_FIRMWARE_INCLUDE_OC=n#CONFIG_FIRMWARE_INCLUDE_OC=y#' build_firmware_modify
+# 此处填写对应频率的十六进制：1000Mhz=0x312 1100Mhz=0x362 1120Mhz=0x372 1200Mhz=0x3B2
+# sed -i 's#CONFIG_FIRMWARE_MT7621_OC="0x312"#CONFIG_FIRMWARE_MT7621_OC="0x362"#' build_firmware_modify
+
+
 # echo "CONFIG_FIRMWARE_INCLUDE_WIREGUARD=y" >> configs/templates/RM2100.config
 
 # sed -i 's#WIREGUARD=n#WIREGUARD=y#' configs/templates/RM2100.config
@@ -19,10 +27,7 @@ sed -i 's#FRPC=n#FRPC=y#' build_firmware_modify
 sed -i 's#ALDRIVER=y#ALDRIVER=n#' build_firmware_modify
 
 sed -i 's#192.168.2#192.168.5#' user/shared/defaults.h
-# 超频
-# sed -i 's#CONFIG_FIRMWARE_INCLUDE_OC=n#CONFIG_FIRMWARE_INCLUDE_OC=y#' build_firmware_modify
-#此处填写对应频率的十六进制：1000Mhz=0x312 1100Mhz=0x362 1120Mhz=0x372 1200Mhz=0x3B2
-# sed -i 's#CONFIG_FIRMWARE_MT7621_OC="0x312"#CONFIG_FIRMWARE_MT7621_OC="0x362"#' build_firmware_modify
+
 
 # 去掉优酸乳
 sed -i 's#CONFIG_FIRMWARE_INCLUDE_SHADOWSOCKS=y#CONFIG_FIRMWARE_INCLUDE_SHADOWSOCKS=n#' build_firmware_modify
