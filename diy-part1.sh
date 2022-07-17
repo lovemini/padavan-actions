7b55d1e12eb6a46aa0c2e8fad886acfa5d897008
LaiQE <laiqe@qq.com>
2022-07-18 02:28:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,10 +6,10 @@ sed -i '/TUNSAFE/d' configs/templates/RM2100.config
 
 # 超频:Padavan-KVR
 # 此处填写对应频率的十六进制：1000Mhz=0x312 1100Mhz=0x362 1120Mhz=0x372 1200Mhz=0x3B2
-sed -i 's#0xc2#0x312#' linux-3.4.x/arch/mips/rt2880/init.c
-echo "CONFIG_FIRMWARE_CPU_900MHZ=y" >> configs/templates/RM2100.config
+# sed -i 's#0xc2#0x312#' linux-3.4.x/arch/mips/rt2880/init.c
+# echo "CONFIG_FIRMWARE_CPU_900MHZ=y" >> configs/templates/RM2100.config
 # 超频:padavan-4.4
-# sed -i 's#CONFIG_FIRMWARE_INCLUDE_OC=n#CONFIG_FIRMWARE_INCLUDE_OC=y#' build_firmware_modify
+sed -i 's#CONFIG_FIRMWARE_INCLUDE_OC=n#CONFIG_FIRMWARE_INCLUDE_OC=y#' build_firmware_modify
 # 此处填写对应频率的十六进制：1000Mhz=0x312 1100Mhz=0x362 1120Mhz=0x372 1200Mhz=0x3B2
 # sed -i 's#CONFIG_FIRMWARE_MT7621_OC="0x312"#CONFIG_FIRMWARE_MT7621_OC="0x362"#' build_firmware_modify
 
